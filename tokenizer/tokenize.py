7d4d81319cb8bb72bebe5cd16890403fe0d325b5
minamorl <minamorl@users.noreply.github.com>
2016-02-01 00:18:56 +0900

    split out tests from code basis

---
+++ tokenizer/tokenize.py
@@ -0,0 +1,57 @@
+
+import enum
+
+class Token(enum.Enum):
+    parenthesis = 1
+    space = 2
+    integer = 3
+    operator = 4
+
+
+def consume(s):
+    if s is None:
+        return None, None
+    if len(s) > 1:
+        return s[0], s[1:]
+    elif len(s) == 1:
+        return s[0], None
+    elif len(s) == 0:
+        return None, None
+
+def is_integer(c):
+    if c is not None:
+        return c in "1234567890"
+    return False
+
+class Tokenizer():
+    def tokenize(self, string):
+        current_state = 0
+        tokens = []
+        temp = ""
+        while True:
+            x, xs = consume(string)
+            string = xs
+
+
+            if current_state is Token.integer:
+                if not is_integer(x):
+                    tokens.append(temp)
+                    temp = ""
+            
+            if x is None:
+                return tokens
+
+            if is_integer(x):
+                current_state = Token.integer
+                temp += x
+
+            if x in "+/-*":
+                current_state = Token.operator
+                tokens.append(x)
+
+            if x in "()":
+                current_state = Token.parenthesis
+                tokens.append(x)
+
+
+
